7c33ceadae298a4d13f2fdedc007be667f61ebb9
baumj-UW <45178573+baumj-UW@users.noreply.github.com>
2018-11-19 10:52:57 -0800

    Update of Project Files

---
+++ MultiMachStudy.py
@@ -0,0 +1,28 @@
+'''
+Created on Nov 18, 2018
+
+@author: baumj
+
+Multimachine Stability Study 
+Power System Dynamics - EE 552
+
+Based on Bergen and Vittal Example 14.6, p. 570
+'''
+
+import math     # Math functions 
+import cmath    # Complex math function conj, rect
+import openpyxl # Methods to read and write xlsx files
+import numpy    # Methods for linear algebra
+
+# Create Ybus matrix
+
+Ybus = numpy.array([[-12.5, 0, 0, 12.5, 0, 0, 0, 0],
+                    [0, -5.556, 0, 0, 5.556, 0, 0, 0],
+                    [0, 0, -8.333, 0, 0, 8.333, 0, 0],
+                    [12.5, 0, 0, -32.48, 10, 0, 10, 0],
+                    [0,]
+                    [0,]
+                    [0,]
+                    [0,]])  #append definition with dtype=complex
+
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# PSDynamics-Study
