a4d9b78012f49b89690e6c2bd859337bbd494160
baumj-UW <45178573+baumj-UW@users.noreply.github.com>
2018-11-24 18:24:59 -0800

    Added scipy numerical integration steps comments

--- MultiMachStudy.py
+++ MultiMachStudy.py
@@ -7,28 +7,41 @@ Multimachine Stability Study
 Power System Dynamics - EE 552
 
 Based on Bergen and Vittal Example 14.6, p. 570
+Machowski pdf p. 486, 3rd order model
 '''
 
 import math     # Math functions 
 import cmath    # Complex math function conj, rect
 import openpyxl # Methods to read and write xlsx files
 import numpy    # Methods for linear algebra
-from test.test_enumerate import EnumerateStartTestCase
+from scipy.integrate import odeint
+
 
 # Create Ybus matrix
+# 
+# Ybus = numpy.array([[-12.5, 0, 0, 12.5, 0, 0, 0, 0],
+#                     [0, -5.556, 0, 0, 5.556, 0, 0, 0],
+#                     [0, 0, -8.333, 0, 0, 8.333, 0, 0],
+#                     [12.5, 0, 0, -32.48, 10, 0, 10, 0],
+#                     [0,]
+#                     [0,]
+#                     [0,]
+#                     [0,]])  #append definition with dtype=complex
 
-Ybus = numpy.array([[-12.5, 0, 0, 12.5, 0, 0, 0, 0],
-                    [0, -5.556, 0, 0, 5.556, 0, 0, 0],
-                    [0, 0, -8.333, 0, 0, 8.333, 0, 0],
-                    [12.5, 0, 0, -32.48, 10, 0, 10, 0],
-                    [0,]
-                    [0,]
-                    [0,]
-                    [0,]])  #append definition with dtype=complex
 
+# Step 2 Add model of load admittances
 
-#someone came in and edited the master file!
+# Step 3 calculate internal gen voltages
 
-#Now I have my own edits which will be saved...
+# Step 4 cacluate prefault / fault / post fault admittance matrices 
 
-#clearing comments
+# Step 5 Kron reduction 
+#===============================================================================
+# Solve Equations with scipy.integrate.odeint 
+# M*delta(w') = Pm - Pe - D*delta(w)
+# d' = delta(w)
+# Ttransdo*Etransq' = Ef - Etransq + Id*(Xd - Xtransd)
+#===============================================================================
+
+ 
+
